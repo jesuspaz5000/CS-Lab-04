5aaaec121c31618e68ecdff19382cb7d69f2b8a4
Quispe Bejar Garlet Analy <64673450+AnalyQuispe@users.noreply.github.com>
2025-09-21 00:42:52 +0000

    Prueba de Garlet.

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,4 +1,9 @@
+import { PassThrough } from "stream";
+
+
 export const allowedUsers = [
     { username: "user1", password: "password1" },
     { username: "user2", password: "password2" },
+    { username: "", Password: "password3" }, "El username vacío no deberia ser invalido",
+    { username: "gquispe", password: "admin123" },
 ];
